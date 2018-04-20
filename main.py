43963a0f5d04f3eb01a3a98f8e11402fbed73903
root <wq2a@mtmail.mtsu.edu>
2018-04-20 19:21:49 +0800

    add module and handle

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 import web
+from handle import Handle
 
 urls = (
   '/.*', 'Handle',
 )
 
-class Handle(object):
-  def GET(self):
-    return "hello, this is a test"
+#class Handle(object):
+#  def GET(self):
+#    return "hello, this is a test"
 
 app = web.application(urls, globals(),autoreload=False)
 application = app.wsgifunc()
