088d2af5d37824f2e68fa0ec59b7a743b447efe9
root <wq2a@mtmail.mtsu.edu>
2020-04-03 13:55:01 +0800

    update

--- modules/handle.py
+++ modules/handle.py
@@ -3,28 +3,55 @@
 
 import hashlib
 import web
+import reply
+import receive
 
 class Handle(object):
-    def GET(self):
+#    def GET(self):
+#        try:
+#            data = web.input()
+#            if len(data) == 0:
+#                return "hello, this is handle view"
+#            signature = data.signature
+#            timestamp = data.timestamp
+#            nonce = data.nonce
+#            echostr = data.echostr
+#            token = "yxbswq2a" #请按照公众平台官网\基本配置中信息填写
+#
+#            list = [token, timestamp, nonce]
+#            list.sort()
+#            sha1 = hashlib.sha1()
+#            map(sha1.update, list)
+#            hashcode = sha1.hexdigest()
+#            print "handle/GET func: hashcode, signature: ", hashcode, signature
+#            if hashcode == signature:
+#                return echostr
+#            else:
+#                return ""
+#        except Exception, Argument:
+#            return Argument
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "yxbswq2a" #请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print "Handle Post webdata is ", webData
+   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg):
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                elif recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
             else:
-                return ""
-        except Exception, Argument:
-            return Argument
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
+
